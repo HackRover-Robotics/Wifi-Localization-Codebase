a6ae8d0ce8eb14a8f4745e457b5aedaa67738ea3
Anwar Aminuddin <anwara96@uw.edu>
2020-05-31 19:31:47 -0700

    Testing Phase

--- RSSI_RECORD.py
+++ RSSI_RECORD.py
@@ -5,7 +5,7 @@ import math
 xMax = 2
 yMax = 2
 gridPoints = []
-filename = "output5inch.json"
+filename = "output.json"
 
 recording = True    # setting new points in fingerprint database
 editing = False     # replacing AP RSSI on given X, Y point
@@ -75,30 +75,44 @@ def compareAP(inputAP):
     shortestDistance = float("inf")
 
     for point in gridPoints:
-        compareAP = point['ap']
+        compareAP = point["ap"]
         totalCalculatedDistance = 0.0
-    
-        for i in range(3):  # go through each AP in inputAP (4 APs)
-            inputQualityString = inputAP[i]["quality"].split("/")[0]
-            inputQuality = float(inputQualityString)
+        print("points: " + str(point['x']) + ", " + str(point['y']))
+            
+        for ap in compareAP:  # go through each AP in compareAP (~4 APs)
+            ssid = ap["ssid"]
 
-            compareQualityString = compareAP[i]["quality"].split("/")[0]
-            compareQuality = float(compareQualityString)
+            for inAP in inputAP:    # go through each AP in inputAP
 
-            inputSignal = float(inputAP[i]["signal"])
-            compareSignal = float(compareAP[i]["signal"])
+                inSsid = inAP["ssid"]
+                if(ssid == inSsid): # find same SSID 
 
-            '''
-            point[1] += calculatedDistance[i]^2-0    -------inputAP
-            point[2] = calculatedDistance[1-4]
-            .
-            .
-            .
-            point[n] = calculatedDistance[1-4]
-            which one is the smallest?
-            '''
-            calculatedDistance = (compareQuality - inputQuality)**2 + (compareSignal - inputSignal)**2
-            totalCalculatedDistance += calculatedDistance
+                    print(ssid + " == " + inSsid)
+                    # if(inputAP[i]["ssid"] == compareAP[i]["ssid"]):
+
+                    inputQualityString = inAP["quality"].split("/")[0]
+                    inputQuality = float(inputQualityString)
+
+                    compareQualityString = ap["quality"].split("/")[0]
+                    compareQuality = float(compareQualityString)
+
+                    inputSignal = float(inAP["signal"])
+                    compareSignal = float(ap["signal"])
+
+                    print("\t Quality:" + str(inputQuality) + " " + str(compareQuality))
+                    print("\t Signals:" + str(inputSignal) + " " + str(compareSignal))
+
+                    '''
+                    point[1] += calculatedDistance[i]^2-0    -------inputAP
+                    point[2] = calculatedDistance[1-4]
+                    .
+                    .
+                    .
+                    point[n] = calculatedDistance[1-4]
+                    which one is the smallest?
+                    '''
+                    calculatedDistance = (compareQuality - inputQuality)**2 + (compareSignal - inputSignal)**2
+                    totalCalculatedDistance += calculatedDistance
 
         print(totalCalculatedDistance)
             
@@ -144,14 +158,20 @@ if recording:
             input("press enter to record APs")
 
             result = getAP()
+
+            print(str(x) + ", " + str(y))
             
+            while(len(result) < 4):
+                result = getAP()
+            
+            for ap in result:
+                print(ap["ssid"])
+
             point = {
                 'x': x,
                 'y': y,
                 'ap': result
-            }
-
-            print(point)
+            }          
 
             gridPoints.append(point)
 
@@ -162,8 +182,12 @@ if locating:
     openFile()
     # getAP()
     inputAP = getAP()
+    while(len(inputAP) < 4):
+        inputAP = getAP()
+
     print(inputAP)
     # [{"ssid": "BeMerry", "quality": "64/70", "signal": -46}, {"ssid": "DIRECT-F6-HP ENVY 4520 series", "quality": "42/70", "signal": -68}, {"ssid": "NETGEAR76", "quality": "50/70", "signal": -60}, {"ssid": "xfinitywifi", "quality": "31/70", "signal": -79}]
+    # location = compareAP(inputAP)
     location = compareAP(inputAP)
     print(location)
         
